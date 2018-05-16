195a3e9742fd07b9997121492ec0d1e81afb0896
Pablo Vieira <povieira@gmail.com>
2018-05-16 11:51:53 -0300

    (feat): add canvas & 'draw line' feature

---
+++ drawingtool/canvas.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+
+class Canvas(object):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self._area = []
+        for _ in range(height):
+            self._area.append([c for c in ' ' * width])
+
+    def draw_line(self, x1, y1, x2, y2):
+        if x1 == x2:  # vertical line
+            for i in range(y1 - 1, y2):
+                self._area[i][x1 - 1] = 'x'
+        elif y1 == y2:  # horizontal line
+            for i in range(x1 - 1, x2):
+                self._area[y1 - 1][i] = 'x'
+
+    def __str__(self):
+        canvas_str = '-' * (self.width + 2) + '\n'
+        for line in self._area:
+            canvas_str += '|' + ''.join(line) + '|\n'
+        canvas_str += '-' * (self.width + 2)
+        return canvas_str
---
+++ tests/test_canvas.py
@@ -0,0 +1,42 @@
+from drawingtool.canvas import Canvas
+
+
+def test_canvas_str():
+    expected_str = (
+        '----------------------\n'
+        '|                    |\n'
+        '|                    |\n'
+        '|                    |\n'
+        '|                    |\n'
+        '----------------------'
+    )
+    canvas = Canvas(width=20, height=4)
+    assert str(canvas) == expected_str
+
+
+def test_draw_horizontal_line():
+    expected_str = (
+        '----------------------\n'
+        '|                    |\n'
+        '|xxxxxx              |\n'
+        '|                    |\n'
+        '|                    |\n'
+        '----------------------'
+    )
+    canvas = Canvas(width=20, height=4)
+    canvas.draw_line(1, 2, 6, 2)
+    assert str(canvas) == expected_str
+
+
+def test_draw_vertical_line():
+    expected_str = (
+        '----------------------\n'
+        '|                    |\n'
+        '|                    |\n'
+        '|     x              |\n'
+        '|     x              |\n'
+        '----------------------'
+    )
+    canvas = Canvas(width=20, height=4)
+    canvas.draw_line(6, 3, 6, 4)
+    assert str(canvas) == expected_str
